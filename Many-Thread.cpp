d3c24eb3fa19e40fc1ac6b156fe199722dfde217
Albert C <albertocamacho@dusty.tamiu.edu>
2024-10-12 17:58:29 +0000

    added things we might need on the other files

--- Many-Thread.cpp
+++ Many-Thread.cpp
@@ -8,3 +8,23 @@ struct student{
     int id;
     int passing;
 };
+
+something
+
+//I know we will need this we can just modify later
+int main() {
+  ifstream infile("students.txt");
+  vector<student> students;
+  int id;
+  int passing;
+
+  while(infile >> id >> passing){
+    students.push_back({id, passing});
+  }
+
+  double returnPercent = calculateReturnPercent(students);
+  cout << "NT: Return Percentage: " << returnPercent << "%" << endl;
+
+  return 0;
+
+}
--- One-Thread.cpp
+++ One-Thread.cpp
@@ -9,3 +9,22 @@ struct student{
     int passing;
 };
 
+hmmmm
+
+//I know we will need this we can just modify later
+int main() {
+  ifstream infile("students.txt");
+  vector<student> students;
+  int id;
+  int passing;
+
+  while(infile >> id >> passing){
+    students.push_back({id, passing});
+  }
+
+  double returnPercent = calculateReturnPercent(students);
+  cout << "NT: Return Percentage: " << returnPercent << "%" << endl;
+
+  return 0;
+
+}
