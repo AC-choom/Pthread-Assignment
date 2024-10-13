2960bf1397fadd4776359896c47ad381f8decc44
Albert C <albertocamacho@dusty.tamiu.edu>
2024-10-13 00:24:25 +0000

    added a little bit on one-thread

--- One-Thread.cpp
+++ One-Thread.cpp
@@ -9,7 +9,22 @@ struct student{
     int passing;
 };
 
-hmmmm
+struct thread {
+  vector<student> students;
+  int percent;
+};
+
+//not finished i just copied and pasted the code but was gonna turn it into a void
+void calculatePercent(){
+  int total = students.size();
+  int passingCount = 0;
+  for(int i = 0; i < total; i++){
+    if (students[i].passing == 1) {
+      passingCount++;
+    }
+  }
+  return (double(passingCount) / total) * 100;
+}
 
 //I know we will need this we can just modify later
 int main() {
